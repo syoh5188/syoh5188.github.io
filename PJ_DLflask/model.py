2b429534e1dbf782db02dd5097ddab9984702129
SEYEON OH, VANESSA <102719063+syoh5188@users.noreply.github.com>
2022-07-07 13:20:31 +0900

    Create model.py
    Signed-off-by: SEYEON OH, VANESSA <102719063+syoh5188@users.noreply.github.com>

---
+++ PJ_DLflask/model.py
@@ -0,0 +1,24 @@
+# Importing the libraries
+
+# Data manipulate
+import numpy as np
+import pandas as pd
+
+# Data Visualization
+import matplotlib.pyplot as plt
+
+# Modeling - Machine Learning
+from sklearn.linear_model import LinearRegression
+
+# Modeling - deep learning
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
+
+# Model save
+import pickle
